65e9cbc168ff469c432a7275d3612d1f8a922908
Laura <lgomez25@ucm.es>
2022-12-07 12:41:28 +0100

    va como 43 de pocho

--- ProyectosOGREvc15x86/LauraGómez_SergioBañaP1E3/media/IG2App/BombaTeseladaFS.glsl
+++ ProyectosOGREvc15x86/LauraGómez_SergioBañaP1E3/media/IG2App/BombaTeseladaFS.glsl
@@ -1,10 +1,10 @@
 #version 330 core
 
+in vec2 vUv0;
 uniform sampler2D texturaS;
 uniform sampler2D texturaM;
 uniform sampler2D texturaC;
 
-in vec2 vUv0;
 out vec4 fFragColor;
 
 void main(){
@@ -12,10 +12,11 @@ void main(){
     vec3 colorMetal= vec3(texture(texturaM, vUv0));
     vec3 colorChecker= vec3(texture(texturaC,vUv0));
 
-    if(colorChecker==vec3(1,1,1)){
-        fFragColor= vec4(colorMetal,1.0);
-    }
+if(colorChecker==vec3(1.0,1.0,1.0)){
+       fFragColor= vec4(0.0,1.0,0.0,1.0);
+}
     else{
-        fFragColor=vec4(colorSpace,1.0);
+       fFragColor=vec4(1.0,0.0,0.0,1.0);
     }
+    //fFragColor=vec4(0.0,1.0,0.0,1.0);
 }
