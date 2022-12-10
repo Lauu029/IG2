e6c8a2aa60e5efbe55783eb5f15ce5ed6b3c5507
Laura <lgomez25@ucm.es>
2022-12-10 18:49:02 +0100

    Boff

--- ProyectosOGREvc15x86/LauraGómez_SergioBañaP1E3/media/IG2App/HolesFS.glsl
+++ ProyectosOGREvc15x86/LauraGómez_SergioBañaP1E3/media/IG2App/HolesFS.glsl
@@ -1,16 +1,16 @@
 #version 330 core
 
-uniform sampler2D texturaS;
-uniform sampler2D texturaM; 
-uniform sampler2D texturaC;
+uniform sampler2D texFront;
+uniform sampler2D texBack; 
+uniform sampler2D texturaX;
 
 in vec2 vUv0; // out del vertex shader
 out vec4 fFragColor; // out del fragment shader
 
 void main() {
-	vec3 colorStones = vec3(texture(texturaS, vUv0)); 
-    vec3 colorMetal = vec3(texture(texturaM, vUv0)); 
-    vec3 colorCorrosion = vec3(texture(texturaC, vUv0)); 
+	vec3 colorStones = vec3(texture(texBack, vUv0)); 
+    vec3 colorMetal = vec3(texture(texFront, vUv0)); 
+    vec3 colorCorrosion = vec3(texture(texturaX, vUv0)); 
 	
 	if(colorCorrosion.r <= 0.6){
 		if(gl_FrontFacing)
