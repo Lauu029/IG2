66b4c3c92dbbefcf3909b9f395691a77ed14151a
Laura <lgomez25@ucm.es>
2022-12-10 19:00:18 +0100

    Ay mecachis me tropecé con el ctrl+c

--- ProyectosOGREvc15x86/LauraGómez_SergioBañaP1E3/IG2App/IG2Main.cpp
+++ ProyectosOGREvc15x86/LauraGómez_SergioBañaP1E3/IG2App/IG2Main.cpp
@@ -2,16 +2,16 @@
 #include "IG2App.h"
 #include "checkML.h"
 
-int main(int argc, char *argv[])     
+int main(int argc, char* argv[])
 {
 	IG2App app;
-  try {
-    app.initApp();
-    app.getRoot()->startRendering();
-  }
-  catch (Ogre::Exception& e) {
-    Ogre::LogManager::getSingleton().logMessage("An exception has occured: " + e.getFullDescription() + "\n");
-  }
-  app.closeApp();
-  return 0;
+	try {
+		app.initApp();
+		app.getRoot()->startRendering();
+	}
+	catch (Ogre::Exception& e) {
+		Ogre::LogManager::getSingleton().logMessage("An exception has occured: " + e.getFullDescription() + "\n");
+	}
+	app.closeApp();
+	return 0;
 }
