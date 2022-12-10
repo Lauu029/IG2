f9d9941321fa7d205742d6790b3dd0a55a209d8f
Sebana02 <sebana@ucm.es>
2022-12-10 18:07:02 +0100

    ejercicio 8 toma ya laura

--- ProyectosOGREvc15x86/LauraGómez_SergioBañaP1E3/media/IG2App/SpaceVS.glsl
+++ ProyectosOGREvc15x86/LauraGómez_SergioBañaP1E3/media/IG2App/SpaceVS.glsl
@@ -1,5 +1,7 @@
 #version 330 core
 
+uniform float ZF;
+uniform float sintime;
 in vec4 vertex;
 in vec2 uv0;
 uniform mat4 modelViewProjectionMatrix;
@@ -7,10 +9,13 @@ out vec2 vUv0;
 out vec2 vUv1;
 
 void main(){
-    vUv0=uv0;
+    vUv0 = uv0;
     vUv1 = uv0;
-    float ZF = 0.5;    
-    vUv1.s=(uv0.s-0.5)*ZF+0.5;
-    vUv1.t=(uv0.t-0.5)*ZF+0.5;
+
+    float ZFF = sintime * 0.25 + 0.75;
+
+    vUv1.s=(uv0.s-0.5)*ZFF+0.5;
+    vUv1.t=(uv0.t-0.5)*ZFF+0.5;
+
     gl_Position= modelViewProjectionMatrix*vertex;
 }
