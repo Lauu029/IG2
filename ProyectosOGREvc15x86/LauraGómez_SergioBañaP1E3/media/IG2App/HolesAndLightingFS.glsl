340fbb5a81a9f9c7f4254755ce4b4e414232e261
Sebana02 <sebana@ucm.es>
2022-12-13 19:17:03 +0100

    a

--- ProyectosOGREvc15x86/LauraGómez_SergioBañaP1E3/media/IG2App/HolesAndLightingFS.glsl
+++ ProyectosOGREvc15x86/LauraGómez_SergioBañaP1E3/media/IG2App/HolesAndLightingFS.glsl
@@ -7,8 +7,8 @@ uniform sampler2D texturaC;
 uniform vec3 lightDiffuse;
 uniform vec4 lightPosition;
 
-uniform vec3 ocreSiena;
-uniform vec3 azulCeruleo;
+uniform vec3 materialDiffuseFront;
+uniform vec3 materialDiffuseBack;
 
 in vec2 vUv0; // out del vertex shader
 in vec3 vCamNormal;
@@ -19,7 +19,7 @@ void main() {
 	
 	if(colorCorrosion.r <= 0.6){
 		if(gl_FrontFacing){
-			vec3 colorMetal = vec3(texture(texturaM, vUv0)) * ocreSiena; 
+			vec3 colorMetal = vec3(texture(texturaM, vUv0)) * materialDiffuseFront; 
 			
             vec3 lightDir = normalize(vec3(lightPosition));
 			float diff = max(dot(vCamNormal, lightDir), 0.0); // positive normal
@@ -29,7 +29,7 @@ void main() {
 		}
         else {
             vec3 colorStones = vec3(texture(texturaS, vUv0)); // acceso a téxel
-			colorStones *= azulCeruleo;
+			colorStones *= materialDiffuseBack;
         
 			vec3 lightDir = normalize(vec3(lightPosition));
 			float diff = max(dot(-vCamNormal, lightDir), 0.0); // negative normal
--- ProyectosOGREvc15x86/LauraGómez_SergioBañaP1E3/media/IG2App/HolesAndVertexLightingFS.glsl
+++ ProyectosOGREvc15x86/LauraGómez_SergioBañaP1E3/media/IG2App/HolesAndVertexLightingFS.glsl
@@ -1,19 +1,19 @@
 #version 330 core
-in vec2 vUv0; // out del vertex shader
-in vec3 vFrontColor; // color de la iluminación interpolado
+in vec2 vUv0; 
+in vec3 vFrontColor; 
 in vec3 vBackColor;
 
 out vec4 fFragColor; // out del fragment shader
 
-uniform sampler2D texturaMetal; // tipo sampler2D para texturas 2D checker
-uniform sampler2D texturaStones; // ‐> unidades de textura (int) space
-uniform sampler2D texturaCorrosion; // ‐> unidades de textura (int) metal
+uniform sampler2D texturaM; 
+uniform sampler2D texturaS; 
+uniform sampler2D texturaC; 
 
 void main() {
 
-    vec3 colorMetal = vec3(texture(texturaMetal, vUv0)); // acceso a téxel
-    vec3 colorStones = vec3(texture(texturaStones, vUv0)); // acceso a téxel
-    vec3 colorCorrosion = vec3(texture(texturaCorrosion, vUv0)); // acceso a téxel
+    vec3 colorMetal = vec3(texture(texturaM, vUv0)); 
+    vec3 colorStones = vec3(texture(texturaS, vUv0)); 
+    vec3 colorCorrosion = vec3(texture(texturaC, vUv0));
 
     vec3 color;
 
